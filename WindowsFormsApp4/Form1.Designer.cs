8e15620ea0e1ea32f1e4ab3bef07cdaed7f0f3c9
6882899 <6882899@stud.nau.edu.ua>
2021-02-23 21:43:18 +0200

    Добавьте файлы проекта.

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rB1 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rB2 = new System.Windows.Forms.RadioButton();
+            this.rB3 = new System.Windows.Forms.RadioButton();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(470, 369);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(311, 201);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "-8,8",
+            "2,2",
+            "4,4"});
+            this.comboBox1.Location = new System.Drawing.Point(342, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(385, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Select a:";
+            // 
+            // rB1
+            // 
+            this.rB1.AutoSize = true;
+            this.rB1.Location = new System.Drawing.Point(7, 19);
+            this.rB1.Name = "rB1";
+            this.rB1.Size = new System.Drawing.Size(61, 17);
+            this.rB1.TabIndex = 7;
+            this.rB1.TabStop = true;
+            this.rB1.Text = "sin(x^2)";
+            this.rB1.UseVisualStyleBackColor = true;
+            this.rB1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 263);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Select b:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(333, 249);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Result:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(342, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "x =";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(369, 140);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(379, 246);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 14;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rB3);
+            this.groupBox1.Controls.Add(this.rB2);
+            this.groupBox1.Controls.Add(this.rB1);
+            this.groupBox1.Location = new System.Drawing.Point(151, 263);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(176, 100);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select c:";
+            // 
+            // rB2
+            // 
+            this.rB2.AutoSize = true;
+            this.rB2.Location = new System.Drawing.Point(7, 43);
+            this.rB2.Name = "rB2";
+            this.rB2.Size = new System.Drawing.Size(61, 17);
+            this.rB2.TabIndex = 8;
+            this.rB2.TabStop = true;
+            this.rB2.Text = "sin(x)^2";
+            this.rB2.UseVisualStyleBackColor = true;
+            // 
+            // rB3
+            // 
+            this.rB3.AutoSize = true;
+            this.rB3.Location = new System.Drawing.Point(7, 66);
+            this.rB3.Name = "rB3";
+            this.rB3.Size = new System.Drawing.Size(73, 17);
+            this.rB3.TabIndex = 9;
+            this.rB3.TabStop = true;
+            this.rB3.Text = "sin(x^2)^2";
+            this.rB3.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.checkedListBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "-1,1",
+            "3,3",
+            "5,5"});
+            this.checkedListBox1.Location = new System.Drawing.Point(15, 282);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 75);
+            this.checkedListBox1.TabIndex = 17;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 421);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rB1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rB3;
+        private System.Windows.Forms.RadioButton rB2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string x1 = textBox1.Text;
+            string a1 = comboBox1.Text;
+            string b1 = checkedListBox1.Text;
+            double x = Convert.ToDouble(textBox1.Text);
+            double a = Convert.ToDouble(comboBox1.Text);
+            double b = Convert.ToDouble(checkedListBox1.Text);
+            if (rB1.Checked)
+            {
+                double c = Math.Sin(Math.Pow(x,2));
+                if (x <= a)
+                {
+                    double z = c * Math.Sin(Math.Pow(b, 2)) * x + b * Math.Log10(c * x + a);
+                    textBox2.Text = z.ToString("");
+                }
+                if (x >= a && x < b)
+                {
+                    double z = a + Math.Log10(b * x) - Math.Pow(Math.Sin(a + c * x), 2);
+                    textBox2.Text = z.ToString("");
+                }
+                if (x > b)
+                {
+                    double z = Math.Sqrt(Math.Cos(a + b * x) + c * Math.Pow(x, 2));
+                    textBox2.Text = z.ToString("");
+                }
+            }
+            if (rB2.Checked)
+            {
+                double c = Math.Pow(Math.Sin(Math.Pow(x, 2)), 2);
+                if (x <= a)
+                {
+                    double z = c * Math.Sin(Math.Pow(b, 2)) * x + b * Math.Log10(c * x + a);
+                    textBox2.Text = z.ToString("");
+                }
+                if (x >= a && x < b)
+                {
+                    double z = a + Math.Log10(b * x) - Math.Pow(Math.Sin(a + c * x), 2);
+                    textBox2.Text = z.ToString("");
+                }
+                if (x > b)
+                {
+                    double z = Math.Sqrt(Math.Cos(a + b * x) + c * Math.Pow(x, 2));
+                    textBox2.Text = z.ToString("");
+                }
+            }
+            if (rB3.Checked)
+            {
+                double c = Math.Pow(Math.Sin(x), 2);
+                if (x <= a)
+                {
+                    double z = c * Math.Sin(Math.Pow(b, 2)) * x + b * Math.Log10(c * x + a);
+                    textBox2.Text = z.ToString("");
+                }
+                if (x >= a && x < b)
+                {
+                    double z = a + Math.Log10(b * x) - Math.Pow(Math.Sin(a + c * x), 2);
+                    textBox2.Text = z.ToString("");
+                }
+                if (x > b)
+                {
+                    double z = Math.Sqrt(Math.Cos(a + b * x) + c * Math.Pow(x, 2));
+                    textBox2.Text = z.ToString("");
+                }
+            }
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
